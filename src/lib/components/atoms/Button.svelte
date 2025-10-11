836127c4e907c2248405cecf82da3484d7e18f54
Geert Beuneker <shellpadinteractive@gmail.com>
2025-10-12 00:43:02 +0200

    Made basic button component

---
+++ src/lib/components/atoms/Button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let type: 'button' | 'submit' | 'reset' = 'button';
+	export let disabled: boolean = false;
+	export let variant: 'primary' | 'secondary' | 'danger' = 'primary';
+</script>
+
+<button
+	on:click
+	{type}
+	{disabled}
+	class={`cursor-pointer rounded-lg px-4 py-2 font-medium transition disabled:cursor-not-allowed disabled:opacity-50
+    ${variant === 'primary' ? 'bg-blue-600 text-white hover:bg-blue-700' : ''}
+    ${variant === 'secondary' ? 'bg-gray-200 text-gray-800 hover:bg-gray-300' : ''}
+    ${variant === 'danger' ? 'bg-red-600 text-white hover:bg-red-700' : ''}
+    `}
+>
+	<slot />
+</button>
--- src/routes/character/[slug]/+page.svelte
+++ src/routes/character/[slug]/+page.svelte
@@ -1,9 +1,19 @@
 <script lang="ts">
 	import { page } from '$app/state';
+	import Button from '$lib/components/atoms/Button.svelte';
 
 	$: slug = page.params.slug || '';
+
+	function handleClick() {
+		alert('Button clicked!');
+	}
 </script>
 
 <main>
 	<h1>Character {slug}</h1>
+	<div class="grid-cols-3">
+		<Button on:click={handleClick}>Audio 1</Button>
+		<Button>Audio 1</Button>
+		<Button>Audio 1</Button>
+	</div>
 </main>
