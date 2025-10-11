010eec2d4b034c1b39651844aea9d30dac6fddba
Geert Beuneker <shellpadinteractive@gmail.com>
2025-10-12 00:01:04 +0200

    Initial setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Catan Soundboard</h1>
+
+<div class="grid grid-cols-3">
+	<a href="/character/siegfried">siegfried</a>
+</div>
---
+++ src/routes/character/[slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/state';
+
+	$: slug = page.params.slug || '';
+</script>
+
+<main>
+	<h1>Character {slug}</h1>
+</main>
