a057f79c0b4dca7b6a0ee1bf901b831cb223bd10
Jerryt Dijkstra <narfsonnarf@gmail.com>
2025-10-12 10:33:29 +0200

    Character names edited

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,12 +5,12 @@
 <h1>Catan Soundboard</h1>
 
 <div class="mt-5 grid grid-cols-3 gap-2">
+	<a href="character/bernadette"><Button>Bernadette</Button></a>
+	<a href="character/chi_lu"><Button>Chi Lu</Button></a>
+	<a href="character/liz"><Button>Liz</Button></a>
+	<a href="character/louis"><Button>Louis</Button></a>
+	<a href="character/mara"><Button>Mara</Button></a>
 	<a href="character/siegfried"><Button>Siegfried</Button></a>
-	<a href="character/siegfried"><Button>Siegfried</Button></a>
-	<a href="character/siegfried"><Button>Siegfried</Button></a>
-	<a href="character/siegfried"><Button>Siegfried</Button></a>
-	<a href="character/siegfried"><Button>Siegfried</Button></a>
-	<a href="character/siegfried"><Button>Siegfried</Button></a>
-	<a href="character/siegfried"><Button>Siegfried</Button></a>
-	<a href="character/siegfried"><Button>Siegfried</Button></a>
+	<a href="character/vicky"><Button>Vicky</Button></a>
+	<a href="character/vincent"><Button>Vincent</Button></a>
 </div>
