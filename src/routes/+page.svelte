9ffcd2e7babcb9584f416fa0a4b599db23dba8c2
Geert Beuneker <shellpadinteractive@gmail.com>
2025-10-12 11:25:27 +0200

    fixed some styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,13 @@
 
 <h1>Catan Soundboard</h1>
 
-<div class="mt-5 grid grid-cols-3 gap-2">
-	<a href="character/bernadette"><Button>Bernadette</Button></a>
-	<a href="character/chi_lu"><Button>Chi Lu</Button></a>
-	<a href="character/liz"><Button>Liz</Button></a>
-	<a href="character/louis"><Button>Louis</Button></a>
-	<a href="character/mara"><Button>Mara</Button></a>
-	<a href="character/siegfried"><Button>Siegfried</Button></a>
-	<a href="character/vicky"><Button>Vicky</Button></a>
-	<a href="character/vincent"><Button>Vincent</Button></a>
+<div class="mt-5 grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3">
+	<a href="character/bernadette" class="contents"><Button>Bernadette</Button></a>
+	<a href="character/chi_lu" class="contents"><Button>Chi Lu</Button></a>
+	<a href="character/liz" class="contents"><Button>Liz</Button></a>
+	<a href="character/louis" class="contents"><Button>Louis</Button></a>
+	<a href="character/mara" class="contents"><Button>Mara</Button></a>
+	<a href="character/siegfried" class="contents"><Button>Siegfried</Button></a>
+	<a href="character/vicky" class="contents"><Button>Vicky</Button></a>
+	<a href="character/vincent" class="contents"><Button>Vincent</Button></a>
 </div>
