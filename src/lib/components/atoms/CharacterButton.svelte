a45ca729fbb8d71a76397cb2a65facb02ab0acda
Geert Beuneker <shellpadinteractive@gmail.com>
2025-10-13 21:22:20 +0200

    Made better character buttons

---
+++ src/lib/components/atoms/CharacterButton.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let character: string;
+</script>
+
+<a
+	href={`character/${character}`}
+	title={`${character}`}
+	class="btn-character group relative aspect-video h-20 rounded border-4 border-catan-yellow md:h-32"
+>
+	<div class="btn-character__bg absolute h-full w-full saturate-50 transition-all group-hover:saturate-100"></div>
+	<img src={`/src/lib/assets/images/${character}.png`} alt={character} class="absolute right-0 h-full" />
+	<span
+		class="absolute bottom-1 w-full stroke-black stroke-2 text-center text-xl font-bold capitalize text-catan-yellow drop-shadow md:text-3xl"
+		>{character}</span
+	>
+</a>
+
+<style lang="scss">
+	.btn-character {
+		span {
+			--stroke-color: #110204;
+
+			text-shadow:
+				-1px -1px 0 var(--stroke-color),
+				1px -1px 0 var(--stroke-color),
+				-1px 1px 0 var(--stroke-color),
+				1px 1px 0 var(--stroke-color);
+		}
+		&__bg {
+			background-size: 100% 100%;
+			background-image: url('/src/lib/assets/images/catan.jpg');
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,18 @@
 <script>
-	import Button from '$lib/components/atoms/Button.svelte';
+	import CharacterButton from '$lib/components/atoms/CharacterButton.svelte';
 </script>
 
 <main class="container mx-auto flex flex-col items-center justify-center py-5">
 	<h1 class="mb-5 text-center">Catan Soundboard</h1>
 
-	<div class="grid grid-cols-3 gap-2 font-bold">
-		<a href="character/bernadette" class="contents"><Button>Bernadette</Button></a>
-		<a href="character/chi lu" class="contents"><Button>Chi Lu</Button></a>
-		<a href="character/liz" class="contents"><Button>Liz</Button></a>
-		<a href="character/louis" class="contents"><Button>Louis</Button></a>
-		<a href="character/mara" class="contents"><Button>Mara</Button></a>
-		<a href="character/siegfried" class="contents"><Button>Siegfried</Button></a>
-		<a href="character/vicky" class="contents"><Button>Vicky</Button></a>
-		<a href="character/vincent" class="contents"><Button>Vincent</Button></a>
+	<div class="flex max-w-3xl flex-wrap justify-center gap-2">
+		<CharacterButton character="Bernadette"></CharacterButton>
+		<CharacterButton character="Chi Lu"></CharacterButton>
+		<CharacterButton character="Liz"></CharacterButton>
+		<CharacterButton character="Louis"></CharacterButton>
+		<CharacterButton character="Mara"></CharacterButton>
+		<CharacterButton character="Siegfried"></CharacterButton>
+		<CharacterButton character="Vicky"></CharacterButton>
+		<CharacterButton character="Vincent"></CharacterButton>
 	</div>
 </main>
